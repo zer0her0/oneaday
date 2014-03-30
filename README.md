55504742410cb0886de2887cf0c01f05a4cee941
Andrew Meier <zer0her0@gmail.com>
2014-03-30 14:20:43 -0400

    Just playing.
    Figuring out if I want to use Node.js or Ruby

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ oneaday
 =======
 
 Repo for my Heroku app.
+
+Currently figuring out if I want to write this in Ruby or Node.js.
---
+++ web.js
@@ -0,0 +1,23 @@
+
+var http = require('http');
+
+//figure out if new day
+//
+
+//random notebook/folder
+//# between 1 & x, where x == highest notebook #, currently 17
+
+//random page/file
+//# between 1 & y, where y == highest page value, between 76 & 102
+//step down till it hits a page/file that exists
+
+//generate page
+
+//header
+// One a Day - Book x, Page y.
+
+//body
+// image
+
+//footer
+// Donation links, Creative Commons License, other links
